59768bf73e6987ebf242f98eaad721f537cd7680
Alex <ajlexmaritime@gmail.com>
2023-02-17 19:42:14 +0200

    Added chunks

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
 import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 import Container from './components/Container';
-import HomePage from './views/HomePage/HomePage';
-import NotFoundView from './views/NotFoundView';
-import MovieView from './views/MovieView/MovieView';
+
 import SharedNavBar from './views/SharedNavBar';
 import SharedMoviesLayout from './views/SharedMoviesLayout';
-import LibraryView from './views/LibraryView';
+
 import MovieDetailsView from './views/MovieDetailsView/MovieDetailsView';
 
+const HomePage = lazy(() => import('./views/HomePage/HomePage' /* webpackChunkName: "HomePage" */));
+const MovieView = lazy(() =>
+  import('./views/MovieView/MovieView' /* webpackChunkName: "MovieView" */),
+);
+const LibraryView = lazy(() => import('./views/LibraryView' /* webpackChunkName: "LibraryView" */));
+const NotFoundView = lazy(() =>
+  import('./views/NotFoundView' /* webpackChunkName: "NotFoundView" */),
+);
+/* webpackChunkName: "Home-Page" */
 function App() {
   return (
     <>
       <Container>
-        <Routes>
-          <Route path="/" element={<SharedNavBar />}>
-            <Route index element={<HomePage />} />
+        <Suspense fallback={<h4>LOADING...</h4>}>
+          <Routes>
+            <Route path="/" element={<SharedNavBar />}>
+              <Route index element={<HomePage />} />
 
-            <Route path="movies" element={<SharedMoviesLayout />}>
-              <Route index element={<MovieView />} />
-              <Route path=":movieID" element={<MovieDetailsView />}></Route>
+              <Route path="movies" element={<SharedMoviesLayout />}>
+                <Route index element={<MovieView />} />
+                <Route path=":movieID" element={<MovieDetailsView />}></Route>
+              </Route>
+
+              <Route path="library" element={<LibraryView />} />
+
+              <Route path="*" element={<NotFoundView />} />
             </Route>
-
-            <Route path="library" element={<LibraryView />} />
-
-            <Route path="*" element={<NotFoundView />} />
-          </Route>
-        </Routes>
+          </Routes>
+        </Suspense>
       </Container>
     </>
   );
